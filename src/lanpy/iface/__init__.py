cd14f69635fb618858b715f25cc461a48d8e452b
Sseve <Sseve@github.com>
2025-01-14 14:35:14 +0800

    interface

--- src/lanpy/iface/__init__.py
+++ src/lanpy/iface/__init__.py
@@ -2,10 +2,10 @@
 所有接口类放在此包下
 """
 
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
 
-class ICar(metaclass=ABCMeta):
+class ICar(ABC):
     """
     定义一些抽象方法，要求继承此接口类必须实现这些抽象方法
     """
